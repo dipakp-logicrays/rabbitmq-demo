95356cd4ef852ddc160aafe0c3756b6a7fe9e172
Dipak <dipakp@logicrays.com>
2025-02-13 19:50:41 +0530

    Send Email with order details using message queue

--- Model/Order/Queue/Consumer.php
+++ Model/Order/Queue/Consumer.php
@@ -5,11 +5,19 @@ namespace Logicrays\RabbitMQ\Model\Order\Queue;
 
 use Magento\Framework\Mail\Template\TransportBuilder;
 use Magento\Framework\Serialize\Serializer\Json;
-use Magento\Store\Model\StoreManagerInterface;
 use Psr\Log\LoggerInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Area;
+use Exception;
 
 class Consumer
 {
+    public const SENDER_EMAIL   = 'trans_email/ident_general/email';
+    public const SENDER_NAME   = 'trans_email/ident_general/name';
+    public const SEND_ORDER_DETAILS_EMAIL_TEMPLATE = 'send_order_details/general/email';
+    public const SEND_ORDER_DETAILS__EMAIL_RECIPIENTS = 'send_order_details/general/recipients_emails';
+
     /**
      * @var TransportBuilder
      */
@@ -21,31 +29,31 @@ class Consumer
     private $json;
 
     /**
-     * @var StoreManagerInterface
-     */
-    private $storeManager;
-
-    /**
      * @var LoggerInterface
      */
     private $logger;
 
     /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
      * @param TransportBuilder $transportBuilder
      * @param Json $json
-     * @param StoreManagerInterface $storeManager
      * @param LoggerInterface $logger
+     * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
         TransportBuilder $transportBuilder,
         Json $json,
-        StoreManagerInterface $storeManager,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        ScopeConfigInterface $scopeConfig
     ) {
         $this->transportBuilder = $transportBuilder;
         $this->json = $json;
-        $this->storeManager = $storeManager;
         $this->logger = $logger;
+        $this->scopeConfig = $scopeConfig;
     }
 
     /**
@@ -56,29 +64,51 @@ class Consumer
      */
     public function processMessage(string $message): void
     {
+
         try {
             $orderData = $this->json->unserialize($message);
-            $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/send_order_details.log');
-            $logger = new \Zend_Log();
-            $logger->addWriter($writer);
-            $logger->info('Order details: ' . $message);
-            // $transport = $this->transportBuilder
-            //     ->setTemplateIdentifier('order_details_email_template')
-            //     ->setTemplateOptions([
-            //         'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
-            //         'store' => $this->storeManager->getStore()->getId()
-            //     ])
-            //     ->setTemplateVars([
-            //         'order_data' => $message // Sending raw JSON
-            //     ])
-            //     ->setFrom([
-            //         'email' => 'sales@example.com',
-            //         'name' => 'Sales'
-            //     ])
-            //     ->addTo($orderData['customer_email'])
-            //     ->getTransport();
 
-            // $transport->sendMessage();
+            $this->logger->info('Order details: ' . $message);
+
+            $storeId = $orderData['store_id'];
+
+            $template = $this->scopeConfig->getValue(
+                self::SEND_ORDER_DETAILS_EMAIL_TEMPLATE,
+                ScopeInterface::SCOPE_STORE,
+                $storeId
+            );
+            $senderEmail= $this->scopeConfig->getValue(self::SENDER_EMAIL, ScopeInterface::SCOPE_STORE, $storeId);
+            $senderName = $this->scopeConfig->getValue(self::SENDER_NAME, ScopeInterface::SCOPE_STORE, $storeId);
+            $emails = $this->scopeConfig->getValue(
+                self::SEND_ORDER_DETAILS__EMAIL_RECIPIENTS,
+                ScopeInterface::SCOPE_STORE,
+                $storeId
+            );
+
+            $vars = [
+                'increment_id' => $orderData['increment_id'],
+                'order_data'  => $message,
+            ];
+
+            $transport = $this->transportBuilder->setTemplateIdentifier(
+                $template
+            )->setTemplateOptions(
+                [
+                    'area'  => Area::AREA_FRONTEND,
+                    'store' => $storeId,
+                ]
+            )->setTemplateVars(
+                $vars
+            )->setFrom(
+                [
+                    'email' => $senderEmail,
+                    'name' => $senderName
+                ]
+            )->addTo(
+                $emails
+            )->getTransport();
+            $transport->sendMessage();
+
         } catch (\Exception $e) {
             $this->logger->error('Error processing order details queue: ' . $e->getMessage());
         }
